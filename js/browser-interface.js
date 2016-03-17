7326e10d9292fe2d562148fe9868c2088a2c2a4e
Michelle Brecunier and Mary Warrington <michelle.brecunier@gmail.com>
2016-03-17 11:54:42 -0700

    create new js file to store map api

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,25 +1,76 @@
-// var apiKey = require('./../.env').apiKey;
-// var map;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 8
-//   });
-// }
-//
-// $(document).ready(function() {
-//   // $.get("https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap").then(function(response) {
-//   //   initMap();
-//   //     }).fail(function(error) {
-//   //       console.log(error.responseJSON.message);
-//   //   });
-//   initMap();
-//
+var apiKey = require('./../.env').apiKey;
+
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 18.450, lng: -66.100},
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+      var places = searchBox.getPlaces();
+
+      if (places.length === 0) {
+        return;
+      }
+
+      // Clear out the old markers.
+      markers.forEach(function(marker) {
+        marker.setMap(null);
+      });
+      markers = [];
+
+      // For each place, get the icon, name and location.
+      var bounds = new google.maps.LatLngBounds();
+      places.forEach(function(place) {
+        var icon = {
+          url: place.icon,
+          size: new google.maps.Size(71, 71),
+          origin: new google.maps.Point(0, 0),
+          anchor: new google.maps.Point(17, 34),
+          scaledSize: new google.maps.Size(25, 25)
+        };
+
+        // Create a marker for each place.
+        markers.push(new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        }));
+
+        if (place.geometry.viewport) {
+          // Only geocodes have viewport.
+          bounds.union(place.geometry.viewport);
+        } else {
+          bounds.extend(place.geometry.location);
+        }
+      });
+      map.fitBounds(bounds);
+    });
+  }
 
 
+$(document).ready(function() {
+  $.get("https://maps.googleapis.com/maps/api/js?key=AIzaSyBndGBaNKCgFhrrwhVGk271BWMNVSCn9Us&libraries=places&callback=" + initAutocomplete(), function() {
+    console.log("hey there");
+  });
 
 
-
-
-
+  // $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+  //       console.log(response);
+  //     });
 });

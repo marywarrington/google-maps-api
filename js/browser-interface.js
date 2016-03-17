273509b7e5b8d4470c4e9a723790f52004edad4b
Michelle Brecunier and Mary Warrington <michelle.brecunier@gmail.com>
2016-03-17 10:20:35 -0700

    add initial files with map

---
+++ js/browser-interface.js
@@ -0,0 +1,25 @@
+// var apiKey = require('./../.env').apiKey;
+// var map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: -34.397, lng: 150.644},
+//     zoom: 8
+//   });
+// }
+//
+// $(document).ready(function() {
+//   // $.get("https://maps.googleapis.com/maps/api/js?key=" + apiKey + "&callback=initMap").then(function(response) {
+//   //   initMap();
+//   //     }).fail(function(error) {
+//   //       console.log(error.responseJSON.message);
+//   //   });
+//   initMap();
+//
+
+
+
+
+
+
+
+});
